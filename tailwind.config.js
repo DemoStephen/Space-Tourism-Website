c99372bd389874f1d4cd25b488ec8ce9f1fe358c
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-22 14:05:03 +0100

    trying to fix bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,27 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     backgroundImage: {
-      "home-desktop": "url('/background-home-desktop.jpg')",
-      "home-tablet": "url('/background-home-tablet.jpg')",
-      "home-mobile": "url('/background-home-mobile.jpg')",
+      "home-desktop": "url('/src/assets/home/background-home-desktop.jpg')",
+      "home-tablet": "url('/src/assets/home/background-home-tablet.jpg')",
+      "home-mobile": "url('/src/assets/home/background-home-mobile.jpg')",
 
-      "crew-desktop": "url('./src/assets/crew/background-crew-desktop.jpg')",
-      "crew-tablet": "url('./src/assets/crew/background-crew-tablet.jpg')",
-      "crew-mobile": "url('./src/assets/crew/background-crew-mobile.jpg')",
+      "crew-desktop": "url('/src/assets/crew/background-crew-desktop.jpg')",
+      "crew-tablet": "url('/src/assets/crew/background-crew-tablet.jpg')",
+      "crew-mobile": "url('/src/assets/crew/background-crew-mobile.jpg')",
 
       "destination-desktop":
-        "url('./src/assets/destination/background-destination-desktop.jpg')",
+        "url('/src/assets/destination/background-destination-desktop.jpg')",
       "destination-tablet":
-        "url('./src/assets/destination/background-destination-tablet.jpg')",
+        "url('/src/assets/destination/background-destination-tablet.jpg')",
       "destination-mobile":
-        "url('./src/assets/destination/background-destination-mobile.jpg')",
+        "url('/src/assets/destination/background-destination-mobile.jpg')",
 
       "technology-desktop":
-        "url('./src/assets/technology/background-technology-desktop.jpg')",
+        "url('/src/assets/technology/background-technology-desktop.jpg')",
       "technology-tablet":
-        "url('./src/assets/technology/background-technology-tablet.jpg')",
+        "url('/src/assets/technology/background-technology-tablet.jpg')",
       "technology-mobile":
-        "url('./src/assets/technology/background-technology-mobile.jpg')",
+        "url('/src/assets/technology/background-technology-mobile.jpg')",
     },
     fontFamily: {
       sans: ['"Bellefair", serif'],
