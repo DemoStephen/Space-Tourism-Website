08c6b32f2f63758fc6a7c982a9c83cb14139f74f
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-22 13:27:15 +0100

    Updated classlist dynamically

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/prop-types */
-export default function List({ title }) {
+export default function List({ title, page, view, onChangeView }) {
   return (
-    <li className="text-white text-center md:text-left uppercase py-4 md:py-8 cursor-pointer border-b-2 border-transparent hover:border-b-2 md:hover:border-white">
+    <li
+      onClick={() => onChangeView(page)}
+      className={`text-white text-center md:text-left uppercase py-4 md:py-8 cursor-pointer border-b-2 border-transparent hover:border-b-2 md:hover:border-white ${
+        page === view && "border-b-2 border-white"
+      }`}
+    >
       {title}
     </li>
   );
