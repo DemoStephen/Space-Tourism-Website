f95884a5dc3f60cfdd40c8bd24cf235948252c55
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-24 20:48:35 +0100

    fixed list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
-export default function List({ title, page, view, onChangeView }) {
+export default function List({ title, page, view, onChangeView, setSideBar }) {
   return (
     <li
-      onClick={() => onChangeView(page)}
-      className={`text-white text-center md:text-left uppercase py-4 md:py-8 cursor-pointer border-b-2 border-transparent hover:border-b-2 md:hover:border-white ${
-        page === view && "border-b-2 border-white"
-      }`}
+      onClick={() => {
+        onChangeView(page);
+        setSideBar(false);
+      }}
+      className={`text-white uppercase py-4 md:py-8 cursor-pointer border-b-2 border-transparent hover:border-b-2 md:hover:border-white ${
+        page === view && !setSideBar && "border-b-2 border-white"
+      }  ${setSideBar && "px-4"}`}
     >
       {title}
     </li>
